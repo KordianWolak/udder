20ec9474483400365a62d9b573cdef0905d9f126
KordianWolak <bartoszkordianwolak@gmail.com>
2019-04-16 21:43:22 +0200

    Initial Commit

---
+++ udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  udder
+//
+//  Created by Roronoa Zoro on 16/04/2019.
+//  Copyright © 2019 Bartosz Wolak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
